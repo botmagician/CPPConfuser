674af6f1d31e6fb1d3c47f8b08478f2dca8c5660
Ryougi-G <1365654168@qq.com>
2020-01-25 23:36:13 +0800

    创建项目并进行基础开发

---
+++ CPPConfuser/Confuser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CPPConfuser
+{
+    class Confuser
+    {
+        List<string> ConfuseRules = new List<string>();
+        public Confuser(string rules)
+        {
+            string[] keyWords = rules.Split('|');
+            for(int i = 0; i < keyWords.Length; i++)
+            {
+                ConfuseRules.Add(keyWords[i]);
+            }
+        }
+        public string DefineConfuse(string source)
+        {
+            List<string> AlreadyFind = ConfuseRules;
+            string[] Singlewords = source.Split(' ');
+            string result = source;
+            
+            StringBuilder finBuilder = new StringBuilder();
+            for(int i = 0; i < AlreadyFind.Count; i++)
+            {
+                StringBuilder builder = new StringBuilder();
+                for (int j = 1; j <= i + 1; j++)
+                {
+                    builder.Append('_');
+                }
+                finBuilder.Append("\n#define " + builder.ToString() + " " + AlreadyFind[i]+"\n");
+            }
+            for(int i = 0; i < AlreadyFind.Count; i++)
+            {
+                if (AlreadyFind[i].Contains("#")) continue;
+                StringBuilder builder = new StringBuilder();
+                for(int j=1;j<=i+1; j++)
+                {
+                    builder.Append('_');
+                }
+                result=result.Replace(AlreadyFind[i], builder.ToString());
+            }
+            finBuilder.Append(result);
+            return finBuilder.ToString();
+        }
+    }
+}
---
+++ CPPConfuser/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CPPConfuser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string rule = "int|long|string|using|namespace|std|cout|cin|bool|break|case|catch|char|class|const|continue|default|delete|do|double|else|false|float|for|if|new|return|short|sizeof";
+            Confuser confuser = new Confuser(rule);
+            textBox2.Text = confuser.DefineConfuse(textBox1.Text);
+        }
+    }
+}
